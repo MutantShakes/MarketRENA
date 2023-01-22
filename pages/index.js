82d47da7e7b5859224a8d3fcf683099f2743dac9
Muteeb <muteeb.sheikh@iiitb.ac.in>
2023-01-22 14:00:24 +0530

    boilerplate ready

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class MarketPlaceIndex extends Component {
+  render() {
+    return <h1> MARKET PLACE </h1>;
+  }
+}
+
+export default MarketPlaceIndex;
